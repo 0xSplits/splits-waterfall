62df52a29b7a50fe1962bc937e9dac5b05b49eb7
Will Minshew <wminshew@gmail.com>
2022-09-23 16:59:18 -0700

    script deterministic deploy

--- script/WaterfallModuleFactory.s.sol
+++ script/WaterfallModuleFactory.s.sol
@@ -5,12 +5,11 @@ import "forge-std/Script.sol";
 import {WaterfallModuleFactory} from "../src/WaterfallModuleFactory.sol";
 
 contract WaterfallModuleFactoryScript is Script {
-    function setUp() public {}
+    function run() external {
+        uint256 privKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(privKey);
 
-    function run() public {
-        vm.startBroadcast();
-
-        new WaterfallModuleFactory();
+        new WaterfallModuleFactory{salt: keccak256("0xSplits.waterfall.v1")}();
 
         vm.stopBroadcast();
     }
